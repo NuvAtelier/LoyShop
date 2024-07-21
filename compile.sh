6ddfb1851b8c0ddb34301f7962ead97fac454237
Sky Young <OstlerDev@users.noreply.github.com>
2024-07-21 16:07:40 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-mvn clean compile package
+mvn clean compile package -o
 # Copy latest plugin in
+cp target/Shop-1.8.5.0.jar ../paper-test-1.20.6/plugins
 cp target/Shop-1.8.5.0.jar ../paper-test-1.21/plugins
+cp target/Shop-1.8.5.0.jar ../spigot-test-1.21/plugins
 # cp target/Shop-1.8.4.7.jar ../paper-test-1.20.4/plugins
 
 # Startup latest minecraft server version to test
+# cd ../paper-test-1.20.6/
+# rm -r plugins/.paper-remapped
+# java -jar paper-1.20.6-148.jar --nogui
+
 cd ../paper-test-1.21/
 rm -r plugins/.paper-remapped
-java -jar paper-1.21-99.jar --nogui
+# java -jar paper-1.21-44.jar --nogui
+java -Dpaper.log-level=FINE -jar paper-1.21-99.jar --nogui
+
+# cd ../spigot-test-1.21/
+# java -jar spigot-1.21.jar --nogui
