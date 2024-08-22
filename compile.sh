3210f9f0d2417a142c4c4050c4f8f0aef2229765
Sky Young <OstlerDev@users.noreply.github.com>
2024-08-21 23:59:25 -0600

    Test using paper 1.21

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# mvn clean compile package
+mvn clean compile package -o
 # Copy latest plugin in
 cp target/Shop-1.9.0.1.jar ../paper-test-1.20.6/plugins
 cp target/Shop-1.9.0.1.jar ../paper-test-1.21/plugins
@@ -11,9 +11,9 @@ cp target/Shop-1.9.0.1.jar ../spigot-test-1.21/plugins
 # rm -r plugins/.paper-remapped
 # java -jar paper-1.20.6-148.jar --nogui
 
-# cd ../paper-test-1.21/
+cd ../paper-test-1.21/
 # rm -r plugins/.paper-remapped
-# java -jar paper-1.21-99.jar --nogui
+java -jar paper-1.21-123.jar --nogui
 
-cd ../spigot-test-1.21/
-java -jar spigot-1.21.jar --nogui
+# cd ../spigot-test-1.21/
+# java -jar spigot-1.21.jar --nogui
